3a0fa8aab52bca12412942042acd27602abd076f
Hashim-ITMTB <hashim.khan83029@gmail.com>
2024-10-16 16:55:44 +0530

    upd: check

--- test.py
+++ test.py
@@ -15,8 +15,16 @@ def handle_captcha(sb):
 
 def main():
     
-    with SB(uc=True, agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36", ad_block=True , incognito=True, is_mobile=True, undetectable=True) as sb:
+    chrome_args = [
+        "--disable-extensions",  # Disable Chrome extensions
+        "--no-sandbox",  # Needed for running in EC2
+        "--disable-infobars",  # Disable infobars
+        "--disable-gpu",  # Disable GPU acceleration
+        # "--headless",  # Uncomment if running in headless mode
+        ]
+    with SB(uc=True, agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36", ad_block=True , incognito=True, is_mobile=True, undetectable=True, chromium_arg=chrome_args) as sb:
         # Open the URL with a reconnect attempt
+        
         sb.uc_open_with_reconnect("https://modijiurl.com/fBUFTv", 3)
 
         if sb.is_element_visible('input[value*="Verify"]'):
