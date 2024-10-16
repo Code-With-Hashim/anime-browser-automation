b3d25099915c31da13b79bd73d53792103ce12ec
Hashim-ITMTB <hashim.khan83029@gmail.com>
2024-10-16 17:09:41 +0530

    upd: work

--- test.py
+++ test.py
@@ -15,21 +15,20 @@ def handle_captcha(sb):
 
 def main():
     
-    chrome_args = [
-        "--disable-extensions",  # Disable Chrome extensions
-        "--no-sandbox",  # Needed for running in EC2
-        "--disable-infobars",  # Disable infobars
-        "--disable-gpu",  # Disable GPU acceleration
-        # "--headless",  # Uncomment if running in headless mode
-        ]
-    with SB(uc=True, agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36", ad_block=True , incognito=True, is_mobile=True, undetectable=True, chromium_arg=chrome_args) as sb:
+    with SB(uc=True, agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36", ad_block=True , incognito=True, is_mobile=True,) as sb:
         # Open the URL with a reconnect attempt
-        
         sb.uc_open_with_reconnect("https://modijiurl.com/fBUFTv", 3)
 
+        current_tabs = sb.driver.window_handles  # Get list of open tabs
+        for tab in current_tabs:
+            sb.switch_to_window(tab)
+            if "modijiurl.com" in sb.get_current_url():  # Check if it's the correct tab
+                break
+        
         if sb.is_element_visible('input[value*="Verify"]'):
             sb.uc_click('input[value*="Verify"]')
             print("clickable")
+
         
         # Print the current URL
         print("Current URL:", sb.get_current_url())
