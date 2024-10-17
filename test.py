ef6617217666ffa8f6c1d9136fee9980ff621410
Hashim-ITMTB <hashim.khan83029@gmail.com>
2024-10-17 10:53:47 +0530

    upd: check

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
+import os
 from seleniumbase import SB
 import time
+import pyautogui
+import random
+from pyvirtualdisplay.display import Display
+disp = Display(visible=True, size=(1366, 768), backend="xvfb", use_xauth=True)
+disp.start()
+
+import Xlib.display
+pyautogui._pyautogui_x11._display = Xlib.display.Display(os.environ['DISPLAY'])
 
 def verify_success(sb):
     sb.assert_element('img[alt="Logo Assembly"]', timeout=4)
     sb.sleep(3)
 
 ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-with SB(uc=True, headed=True, disable_features="UserAgentClientHint", agent=ua) as sb:
+with SB(uc=True, headed=True, disable_features="UserAgentClientHint", agent=ua, incognito=True) as sb:
     sb.uc_open_with_reconnect("https://modijiurl.com/fBUFTv", 3)
 
     print(sb.get_current_url())
@@ -14,17 +23,17 @@ with SB(uc=True, headed=True, disable_features="UserAgentClientHint", agent=ua) 
         # For Firefox, use:
         # driver_version = self.driver.capabilities['moz:geckodriverVersion']
         
-    try:
-        verify_success(sb)
-    except Exception:
-        if sb.is_element_visible('input[value*="Verify"]'):
-            sb.uc_click('input[value*="Verify"]')
-        else:
-            sb.uc_gui_click_captcha()
-        try:
-            verify_success(sb)
-        except Exception:
-            print("error")
-            pass
-    time.slee(10)
+    if sb.is_element_visible("iframe"):
+        sb.switch_to_frame("iframe")
+        sb.execute_script('document.querySelector("input").focus()')
+        sb.disconnect()
+        print('Click..')
+        pyautogui.press(" ")
+        sb.driver.reconnect(10)
+    random_number = random.randint(1000, 9999)
+    filename = f"screenshot_{random_number}.png"
+    sb.save_screenshot(filename)
+    print('End.')
+
+    time.sleep(10)
     print(sb.get_current_url())
